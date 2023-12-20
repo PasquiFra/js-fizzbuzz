9f180118b588c28c3d459855d5b4aa87bb2b5680
Francesco <francesco.pasquinoni@hotmail.it>
2023-12-20 16:13:55 +0100

    Generated numbers from 1 to 100

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
 console.log("JS OK");
 
+// Definisco le variabili
+
+const divGen = document.createElement ("div");
+
+
+// creo la formula "for" per generare i valori da 1 a 100
+
+for (let i = 0; i < 100; i++) {
+    const numberGen = i + 1;
+    console.log("i= ", i, "Numero generato", i + 1);
+}
+
